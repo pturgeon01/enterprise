8b6bc908ee3fe0df26e6cab4ad9e00fdd65815ce
pturgeon01 <philippe.turgeon@hotmail.com>
2025-03-24 12:13:47 +0100

    Update DOF_Adjustments.py

--- enterprise/signals/DOF_Adjustments.py
+++ enterprise/signals/DOF_Adjustments.py
@@ -14,3 +14,16 @@ gs_eff = np.array(gs_eff)
 T = np.array(T)
 
 @function
+def return_DOF(input_value, T, geeff):
+    # Convert T to a NumPy array (if not already)
+    T = np.array(T) #in eV
+    geeff = np.array(geeff)
+    # Find the index of the closest value in T to input_value
+    input_value = np.array(input_value, ndmin=1)
+    closest_indices = np.zeros(len(input_value), dtype=int)
+    for i,v in enumerate(input_value):
+        if np.abs(T[i] - v) < 1e40:
+            closest_indices[i] = np.argmin(np.abs(T - v))
+        else:
+            closest_indices[i] = 1710 
+    return ge_eff[closest_indices]
