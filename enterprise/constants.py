064b66c789b2790240475e4842b17030630bfa4f
pturgeon01 <philippe.turgeon@hotmail.com>
2025-03-27 17:28:23 +0100

    Update constants.py

--- enterprise/constants.py
+++ enterprise/constants.py
@@ -7,11 +7,12 @@ scipy.constants for physical constants.
 import numpy as np
 import scipy.constants as sc
 # Cosmological parameters used
-h = 0.67 #1801.04268
+hc = 0.67 #1801.04268
 Om_Mat = 0.344 #2303.10095
 Om_Rad = 2.47 * 10**(-5) / h**2 #1801.04268
 H_0 = 68.4 #In kms-1Mpc-1 2412.13045
 T_0 = 2.35* 10**(-4) #In eV 
+A_s = (np.e**(3.053))/(10**10) #Planck data PL21+BK18+LV21 2208
 # mathematical constants from numpy
 # the log constants are useful for converting between log base 10 and e
 pi = np.pi
