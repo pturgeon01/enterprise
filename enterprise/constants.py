aab30fcaf1eb057511ad3c18e3d4e43266be7825
pturgeon01 <philippe.turgeon@hotmail.com>
2025-03-28 13:47:48 +0100

    Update constants.py

--- enterprise/constants.py
+++ enterprise/constants.py
@@ -13,6 +13,7 @@ Om_Rad = 2.47 * 10**(-5) / hc**2 #1801.04268
 H_0 = 68.4 #In kms-1Mpc-1 2412.13045
 T_0 = 2.35* 10**(-4) #In eV 
 A_s = (np.e**(3.053))/(10**10) #Planck data PL21+BK18+LV21 2208
+DelN = 0.4 #
 # mathematical constants from numpy
 # the log constants are useful for converting between log base 10 and e
 pi = np.pi
