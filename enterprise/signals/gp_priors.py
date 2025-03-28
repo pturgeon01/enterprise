d9ff4f3cc309df0e52d20f28924e3024bc531262
pturgeon01 <philippe.turgeon@hotmail.com>
2025-03-28 13:58:13 +0100

    Update gp_priors.py

--- enterprise/signals/gp_priors.py
+++ enterprise/signals/gp_priors.py
@@ -53,9 +53,8 @@ def custom_powerlaw(f, log10_r=-1.6, n_t=6, log10_T_rh=9, log10_f_inf=10, compon
 #BBN prior function
 
 @function
-def BBN_prior(f, log10_r=-1.6, n_t=6, log10_T_rh=9, log10_f_inf=10, components=2):
-    df = np.diff(np.concatenate((np.array([0]),f[::components])))
-    if const.hc**2 * it.quad(lambda x: Power_Spectrum(x, log10_r=log10_r, n_t= n_t) * Transfer_function(x, log10_T_rh=log10_T_rh, log10_f_inf=log10_f_inf) * 1 / 12 *(2*np.pi*x)**2 / (const.H_0*1000)**2 /x, const.f_BBN, 10**(log10_f_inf))[0]*np.repeat(df,components) > 5.6*10**(-6)*const.DelN*np.repeat(df,components):
+def BBN_prior(log10_r=-1.6, n_t=6, log10_T_rh=9, log10_f_inf=10, components=2):
+    if const.hc**2 * it.quad(lambda x: Power_Spectrum(x, log10_r=log10_r, n_t= n_t) * Transfer_function(x, log10_T_rh=log10_T_rh, log10_f_inf=log10_f_inf) * 1 / 12 *(2*np.pi*x)**2 / (const.H_0*1000)**2 /x, const.f_BBN, 10**(log10_f_inf))[0]) > 5.6*10**(-6)*const.DelN:
         #1 is much larger than the standard output
         p = np.ones(len(df))
     else:
